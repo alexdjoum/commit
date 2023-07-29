9809fa484fd921d174f84af9588d13fd769e962d
alexmartial <kammegnealex@gmail.com>
2023-07-29 08:07:18 +0100

    Commit automatique du 29/07/2023

--- commit.js
+++ commit.js
@@ -1,16 +1,37 @@
 const { exec } = require('child_process');
 
-function commitAndPush() {
-  const date = new Date().toLocaleDateString('fr-FR');
-  const commitMessage = `Commit automatique du ${date}`;
 
-  exec(`git add . && git commit -m "${commitMessage}" && git push`, (err, stdout, stderr) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(stdout);
-  });
-}
+const http = require('http');
 
-setInterval(commitAndPush, 86400000); // exécute la fonction toutes les 24 heures (en millisecondes)
+const server = http.createServer((req, res) => {
+
+  function commitAndPush() {
+    console.log("Je vérifie ====>>>")
+    const date = new Date().toLocaleDateString('fr-FR');
+    const commitMessage = `Commit automatique du ${date}`;
+
+    exec(`git add . && git commit -m "${commitMessage}" && git push`, (err, stdout, stderr) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(stdout);
+    });
+  }
+
+  setInterval(commitAndPush(), 86400000);
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!');
+
+
+
+});
+
+server.listen(3000, 'localhost', () => {
+  console.log('Le serveur est en cours d\'écoute sur le port 3000');
+});
+
+
+ // exécute la fonction toutes les 24 heures (en millisecondes)
