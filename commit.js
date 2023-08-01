0591198d923f70121d81b3b0d7b95d704f72835a
alexmartial <kammegnealex@gmail.com>
2023-08-01 04:58:58 +0100

    Commit automatique du 01/08/2023

--- commit.js
+++ commit.js
@@ -1,8 +1,8 @@
+const cron = require('node-cron');
 const { exec } = require('child_process');
 
-
-const http = require('http');
-
+cron.schedule('53 4 * * *', commitAndPush
+);
 function commitAndPush() {
     const date = new Date().toLocaleDateString('fr-FR');
     const commitMessage = `Commit automatique du ${date}`;
@@ -15,25 +15,6 @@ function commitAndPush() {
         console.log(stdout);
     });
 }
+setInterval(commitAndPush, 90000);
 
 
-
-const server = http.createServer((req, res) => {
-
-
-  res.statusCode = 200;
-  console.log('mon test est passé')
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!');
-  setInterval(commitAndPush, 90000);
-
-
-
-});
-
-server.listen(3000, 'localhost', () => {
-  console.log('Le serveur est en cours d\'écoute sur le port 3000');
-});
-
-
- // exécute la fonction toutes les 24 heures (en millisecondes)
